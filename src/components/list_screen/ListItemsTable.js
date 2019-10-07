2ebe8460b4a8492d8dc22d98a011c23a66480c0d
Xin Han Huang <xinha.huang@stonybrook.edu>
2019-10-07 15:45:41 -0400

    fixed the header

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -4,7 +4,8 @@ import ListItemCard from './ListItemCard'
 export class ListItemsTable extends Component {
     render() {
         return (
-            <div id="list_items_container">
+            <div id="list_items_container" className='list_item_header_card'>
+                <div className='list_item_header_card'></div>
                 <div className="list_item_task_header">Task</div>
                 <div className="list_item_due_date_header">Due Date</div>
                 <div className="list_item_status_header">Status</div>
